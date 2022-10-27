8585fc2a62d12519c59b909a9b34aefadd332885
Jin Chen <jin7157@yahoo.com.tw>
2022-10-27 13:18:32 +0800

    [feat] Add frames per second tool.

---
+++ Assets/Pro Standard Assets/Library/Analysis/FramesPerSecond.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Library
+{
+    public class FramesPerSecond : MonoBehaviour
+    {
+        private const int ZERO = 0;
+        private const int INTERVAL_TIME = 1;
+
+        [SerializeField] private Text text;
+
+        private int frames;
+        private float timer;
+
+        void Update()
+        {
+            frames++;
+            timer += Time.unscaledDeltaTime;
+
+            if (timer >= INTERVAL_TIME)
+            {
+                text.text = frames.ToString();
+                frames = ZERO;
+                timer -= INTERVAL_TIME;
+            }
+        }
+    }
+}
